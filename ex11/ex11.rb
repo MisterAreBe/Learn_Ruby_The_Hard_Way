d416009be73d2d479df5595f9d65188a8933bd4f
Ricky <richardcollins52317@gmail.com>
2018-06-06 20:36:16 -0400

    Exercise 11

---
+++ ex11/ex11.rb
@@ -0,0 +1,8 @@
+print "How old is ye, boy? "
+age = gets.chomp
+print "How tall are ya? "
+height = gets.chomp
+print "How big yer gut? "
+weight = gets.chomp
+
+puts "So, you is #{age} old, #{height} tall, and #{weight} big huh?"
