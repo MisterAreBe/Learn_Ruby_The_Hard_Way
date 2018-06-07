206726e653b5787774b6065d7440de93034b8b00
Ricky <richardcollins52317@gmail.com>
2018-06-06 20:29:53 -0400

    exercise5

---
+++ ex5/ex5.rb
@@ -0,0 +1,20 @@
+name = "Richard L. Collins"
+age = 22
+height = 60
+weight = 240
+eyes_hair = "Brown"
+teeth = "Yellowish"
+centimeters = 3
+ounces = 16
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Yea, that's a little overweight, but it's okay."
+puts "He's got #{eyes_hair} eyes and #{eyes_hair} hair."
+puts "His teeh are now #{teeth}, from smoking and poor dental hygene."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "You could also say that I weight #{weight * ounces} ounces."
+puts "Or you could say that I am #{height * centimeters} centimeters tall."
