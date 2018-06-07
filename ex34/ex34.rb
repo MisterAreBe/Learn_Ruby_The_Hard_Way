7701b71de6c11c2b87f8a3557a8725ea91956c75
Ricky <richardcollins52317@gmail.com>
2018-06-06 21:01:07 -0400

    Exercise 34

---
+++ ex34/ex34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+ordinal = [0, 1, 2, 3, 4, 5]
+cardinal = ['1st', '2nd', '3rd', '4th', '5th', '6th']
+
+puts "The animal at 1 is a #{animals[1]}"
+puts "The third (3rd) animal is at 2 and is a #{animals[2]}"
+puts "The first (1st) animal is at 1 and is a #{animals[0]}"
+puts "The animal at 3 is a #{animals[3]}"
+puts "The fifth (5th) animal is at 4 and is a  #{animals[4]}"
+puts "The animal at 2 is a #{animals[2]}"
+puts "The sixth (6th) animal is at 5 and is a #{animals[5]}"
+puts "The animal at 4 is a #{animals[4]}"
+
+puts "Did you know that the ordinal number #{ordinal[0]}"
+puts "Is the same as the cardinal number #{cardinal[0]}"
+puts "Did you know that the ordinal number #{ordinal[1]}"
+puts "Is the same as the cardinal number #{cardinal[1]}"
+puts "Did you know that the ordinal number #{ordinal[2]}"
+puts "Is the same as the cardinal number #{cardinal[2]}"
+puts "Did you know that the ordinal number #{ordinal[3]}"
+puts "Is the same as the cardinal number #{cardinal[3]}"
+puts "Did you know that the ordinal number #{ordinal[4]}"
+puts "Is the same as the cardinal number #{cardinal[4]}"
+puts "Did you know that the ordinal number #{ordinal[5]}"
+puts "Is the same as the cardinal number #{cardinal[5]}"
